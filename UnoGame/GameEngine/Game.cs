e061a1470b5d3714eb325828f07e12bb6346b733
Andrei Pestov <anpest@taltech.ee>
2023-10-26 21:46:04 +0300

    Menu first try!

---
+++ UnoGame/GameEngine/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace GameEngine;
+
+public class Game
+{
+    // Players And Cards Dictionary
+    public int PlayersCount { get; set; } = default!;
+    
+}
+
---
+++ UnoGame/MenuSystem/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MenuSystem;
+
+public class MenuItem
+{
+    public string Shortcut { get; set; }
+    
+    public string Label { get; set; }
+    
+    public MenuItem(string shortcut, string label)
+    {
+        Shortcut = shortcut;
+        Label = label;
+    }
+}
